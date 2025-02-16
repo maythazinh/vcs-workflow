5a6732b00b570a345e317e4458f971c10cd2b477
MAY THAZIN HLAING <maythazinhlaing@MAY.local>
2025-02-17 01:15:48 +0900

    delete all

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
-resource "aws_vpc" "hello_vpc" {
+# resource "aws_vpc" "hello_vpc" {
 
-  cidr_block           = "192.168.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
+#   cidr_block           = "192.168.0.0/16"
+#   enable_dns_hostnames = true
+#   enable_dns_support   = true
 
-}
+# }
 
 # resource "aws_vpc" "hello_vpc2" {
 
